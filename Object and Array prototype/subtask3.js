f64daa62c943467d48cb41944e1f8bf1b52b6845
MD Liyaqath Ali <Md_Liyaqath_Ali@epam.com>
2022-04-20 05:06:11 +0530

    changes made

--- Object and Array prototype/subtask3.js
+++ Object and Array prototype/subtask3.js
@@ -1,14 +1,6 @@
 // https://www.codewars.com/kata/function-cache
 // For Problem Statement and Function call open the above link and proceed with --> Select JS as Language Then --> TRAIN
 
-function cache(func) {
-  const cachedResult = {};
-  return (...args) => {
-    const key = JSON.stringify(args);
-    if(!(key in cachedResult)){
-      const result = func.apply(null, args);
-      cachedResult[key] = result;
-    }
-    return cachedResult[key];
-  }
-}
+
+const _ = require('lodash');
+const cache = func => _.memoize(func, JSON.stringify);
