e746dcf930c4052800ab18aacd0e6845fd775b7f
MD Liyaqath Ali <Md_Liyaqath_Ali@epam.com>
2022-05-19 16:54:56 +0530

    node js task 2 done

---
+++ Node js Task2/dataBase/data.js
@@ -0,0 +1,33 @@
+const users = [
+  {
+    id: "452352",
+    login: "Alex",
+    password: "Password12",
+    age: 24,
+    isDeleted: false
+  },
+  {
+    id: "452353",
+    login: "Alice",
+    password: "Password123",
+    age: 27,
+    isDeleted: false
+  },
+  {
+    id: "452354",
+    login: "Jack",
+    password: "Password1234",
+    age: 22,
+    isDeleted: false
+  },
+  {
+    id: "452355",
+    login: "Peter",
+    password: "Password132",
+    age: 32,
+    isDeleted: true
+  },
+];
+module.exports = {
+  users: users,
+};
